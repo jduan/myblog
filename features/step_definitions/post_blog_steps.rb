bf1accd92d5e30fedf072c7e83322e395f442424
Jingjing Duan <jingjing.duan@hulu.com>
2012-11-24 23:13:34 -0800

    first working cucumber test

--- features/step_definitions/post_blog_steps.rb
+++ features/step_definitions/post_blog_steps.rb
@@ -1,11 +1,11 @@
 Given /^a user visits the create blog page$/ do
-  visit blogs_path
+  visit new_blog_path
 end
 
 When /^the user fills the form and submits$/ do
-  fill_in "title", with: "first blog"
-  fill_in "body", with: "I'm starting to build real stuff now!"
-  click_button "submit"
+  fill_in "blog_title", with: "second blog"
+  fill_in "blog_body", with: "I'm starting to build real stuff now!"
+  click_button "Create Blog"
 end
 
 Then /^a blog should have been created in the database$/ do
